3f45b771d6b7a1d75c9010dfcea4dc25e6143ab9
solympe <gamauv@yandex.com>
2019-12-22 10:16:08 +0300

    .

--- test.go
+++ test.go
@@ -1,22 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+type Inter interface {
+	Make()
+}
 
-type Inter interface {}
-
+type elementB struct {
+	surname string
+}
 
 type elementA struct {
+	name string
+	elementB
+}
+
+func (a *elementA) GetName() {
+	fmt.Println("My name is: ", a.name)
+}
+
+func (a *elementA) GetSurname() {
+	fmt.Println("My surname is: ", a.surname)
+}
+
+func (b *elementB) Lals() {
+	fmt.Println("LALS!")
 }
 
 func (e *elementA) Make() {
 	fmt.Println("make make make")
 }
 
+func NewPerson() elementA {
+	return elementA{
+		name:     "John",
+		elementB: elementB{"Govard"},
+	}
+}
 
 func main() {
 	fmt.Println("Test")
 
-	var i int = 5
+	nel := NewPerson()
+
+	nel.GetName()
+	nel.GetSurname()
+	nel.Lals()
 
 }
